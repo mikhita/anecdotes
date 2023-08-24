a0cf24f146a213e71d63d5b3cfa514ab8c808ae5
mikhita <mikheilispiriani@yahoo.com>
2023-08-24 12:21:04 +0400

    changing the initial state to the objects array

---
+++ src/components/Filter.js
@@ -0,0 +1,16 @@
+const Filter = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value)
+  }
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Filter

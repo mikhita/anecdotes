81bfcf60d4ec94639801b7663d7027bbac41ccff
mikhita <mikheilispiriani@yahoo.com>
2023-08-28 12:16:54 +0400

    adding redux toolkit and configurestore in index js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+// import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer
-})
+// const reducer = combineReducers({
+//   anecdotes: anecdoteReducer,
+//   filter: filterReducer
+// })
 
-const store = createStore(reducer)
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer
+  }
+})
 
 console.log(store.getState())
 

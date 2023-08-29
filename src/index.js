7e9d524a5b0e9fab37e89e171d8c596fa3068044
mikhita <mikheilispiriani@yahoo.com>
2023-08-29 15:22:43 +0400

    I create notification and its reducer with createslice function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 // const reducer = combineReducers({
 //   anecdotes: anecdoteReducer,
@@ -15,7 +16,8 @@ import filterReducer from './reducers/filterReducer'
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
-    filter: filterReducer
+    filter: filterReducer,
+    notification: notificationReducer
   }
 })
 
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,37 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const getId = () => (100000 * Math.random()).toFixed(0)
+const getVote = () => (10 * Math.random()).toFixed(0)
+
 
 
 const anecdotesAtStart = [
   {
     content: 'If it hurts, do it more often',
-    votes: getId(),
+    votes: getVote(),
     id: getId()
   },
   {
     content: 'Adding manpower to a late software project makes it later!',
-    votes: getId(),
+    votes: getVote(),
     id: getId()
   },
   {
     content: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    votes: getId(),
+    votes: getVote(),
     id: getId()
   },
   {
     content: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    votes: getId(),
+    votes: getVote(),
     id: getId()
   },
   {
     content: 'Premature optimization is the root of all evil.',
-    votes: getId(),
+    votes: getVote(),
     id: getId()
   },
   {
     content: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    votes: getId(),
+    votes: getVote(),
     id: getId()
   }
 ]
